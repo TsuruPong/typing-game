74cf407bf68dad65fe00ef4e0b5c65acaa77dd4f
TsuruPong <50530188+TsuruPong@users.noreply.github.com>
2024-04-03 00:24:01 +0900

    大まかなレイアウトを修正

--- service/app/src/app/typing-game/page.tsx
+++ service/app/src/app/typing-game/page.tsx
@@ -1,17 +1,20 @@
 
 export default function Home() {
   return (
-    <main className="w-full h-full px-[5%] py-[3%]">
+    <main className="w-full h-full px-[5%] py-[2%]">
       <div className="h-full grid grid-rows-[0.1fr,1fr,0.1fr] grid-flow-row gap-7">
-        <div className="noxnmoneeyo^^">
+        <div className="header">
           header
         </div>
-        <div className="h-full grid grid-rows-3">
-          <div>head</div>
-          <div>main</div>
-          <div>foot</div>
+        <div className="h-full grid grid-rows-[0.7fr,auto]">
+          <div className="sentence">
+            sentence
+          </div>
+          <div className="virtual-keybord">
+            virtual keyboard
+          </div>
         </div>
-        <div className="naxnmoneeyo^^">
+        <div className="footer">
           footer
         </div>
       </div>
