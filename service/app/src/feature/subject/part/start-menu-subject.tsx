07b21fb9046dd37517c70d8117e29c49561c6f5b
TsuruPong <50530188+TsuruPong@users.noreply.github.com>
2024-04-06 16:21:31 +0900

    スタートメニューのレイアウト作成

---
+++ service/app/src/feature/subject/part/start-menu-subject.tsx
@@ -0,0 +1,12 @@
+"use client";
+import * as React from 'react';
+
+export const StartMenuSubject: React.FC = () => {
+    return (
+        <div className='flex items-center'>
+            <div className='mx-auto my-0'>
+                スペースキーを押下でスタート
+            </div>
+        </div>
+    )
+}
