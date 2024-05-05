95f0f51da3708157862f1ea0d6baa83ab583658f
TsuruPong <50530188+TsuruPong@users.noreply.github.com>
2024-05-06 00:55:53 +0900

    ファイル名とか修正

--- service/app/src/components/provider/lib/provider.tsx
+++ service/app/src/components/provider/lib/provider.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import * as React from "react";
-import { ThemeProvider } from "@/libs/next-theme/theme-provider";
-import { NextUiProvider } from "@/libs/nextui/nextui-provider";
+
+import { ThemeProvider } from "@/libs/next-theme/themeProvider";
+import { NextUiProvider } from "@/libs/nextui/nextuiProvider";
 import { CommonParentProps } from "@/types/common/parent/props";
 
 export const AnyProvider: React.FC<CommonParentProps> = ({ children }) => {
